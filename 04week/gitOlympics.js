072aec9908862af44600167b8f129b6219343ec7
Christy Sparrow <32336300+Christy-Sparrow@users.noreply.github.com>
2018-02-06 19:11:32 -0600

    first

---
+++ 04week/gitOlympics.js
@@ -0,0 +1,10 @@
+'use scrict';
+
+const arr = ['michael','paul','bob','tonya','nancy'];
+
+const printListOfOlympians=(arr)=> {
+  arr.forEach(function(ind) {
+    console.log(ind);
+  });
+}
+printListOfOlympians(arr);
